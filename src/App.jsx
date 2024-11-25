fe7f5060d94e660aabba28dcab29ae1e13860719
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2024-11-25 11:26:11 +0545

    Adding react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useState, useContext } from "react";
-
+import { useState, useContext } from "react"
 
 const App = () => {
   const [input, setInput] = useState("");
@@ -8,9 +7,9 @@ const App = () => {
   return (
     <div className="w-full h-screen text-white px-8">
       <nav className="w-full p-3 flex justify-between items-center">
-        <h1 className="font-bold tracking-wide text-3xl">Weather App</h1>
+        <h1 className="text-black font-bold tracking-wide text-3xl">Weather App</h1>
         <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
-          <img src="{}" alt="search" />
+         <img src="" alt="search" />
           <input
             onKeyUp={(e) => {
               if (e.key === "Enter") {
