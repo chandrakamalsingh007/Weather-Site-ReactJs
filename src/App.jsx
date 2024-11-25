d4c14b436c18006dbb547566c1f1fa297e791fa3
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2024-11-25 14:54:51 +0545

    Fetching Data from OpenWeatherAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import { data } from "autoprefixer";
+
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("pokhara");
+
+  useEffect(() => {
+    getWeatherData(city)
+  },[city])
+
+  const getWeatherData = () => {
+    const apiKey = 'fef5789c35085a507050decb49889d42';
+
+    const currentWeatherUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metrics&appid=${apiKey}` ;
+    // const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    fetch(currentWeatherUrl)
+      .then(response => response.json())
+      .then(data => {setWeatherData(data);
+       console.log(JSON.stringify(data))});
+  };
+
+  const handleSearch = (searchedCity) =>{
+    setCity(searchedCity);
+  }
   return (
     <>
-    <Navbar/>
+      <Navbar onSearch={handleSearch} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,48 @@
-import React from 'react'
-import FilterDramaIcon from '@mui/icons-material/FilterDrama';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import GpsFixedIcon from '@mui/icons-material/GpsFixed';
+import React, { useState } from "react";
+import FilterDramaIcon from "@mui/icons-material/FilterDrama";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import GpsFixedIcon from "@mui/icons-material/GpsFixed";
 
-const Navbar = () => {
-  return (
-    <nav className='flex items-center justify-between p-5  '>
-        <div className='flex items-center gap-2 text-lg'>
-            <FilterDramaIcon className='text-[#4B5550]'/>
-            <p className="font-bold text-[#4B5550]">HamroWeather</p>
-        </div>
+const Navbar = ({ onSearch }) => {
+  const [searchCity, setSearchCity] = useState("");
 
-        <div className="flex items-center gap-1">
-        <TextField className='bg-white rounded-3xl w-[22rem] outline-[#4B5550] ' variant="outlined" placeholder="Search city" />
-        <Button variant="contained" style={{backgroundColor:'#4B5550'}} className='rounded-md mt-1 text-base font-bold '>
-            Search
-        </Button>
-        </div>
-
-        <div className='bg-[#4B5550] h-[35px] w-[170px] text-white gap-2 flex items-center rounded-2xl p-1'>
-        <GpsFixedIcon/>
-        <p>Current Location</p>
-        </div>
-    </nav>
-  )
+const handleSearchClick=()=>{
+    if(searchCity.trim()){
+        onSearch(searchCity);
+    }
 }
+  return (
+    <nav className="flex items-center justify-between p-5  ">
+      <div className="flex items-center gap-2 text-lg">
+        <FilterDramaIcon className="text-[#4B5550]" />
+        <p className="font-bold text-[#4B5550]">HamroWeather</p>
+      </div>
 
-export default Navbar
+      <div className="flex items-center gap-1">
+        <TextField
+          className="bg-white rounded-3xl w-[22rem] outline-[#4B5550] "
+          variant="outlined"
+          placeholder="Search city"
+          value={searchCity}
+          onChange={(e)=>setSearchCity(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          style={{ backgroundColor: "#4B5550" }}
+          className="rounded-md mt-1 text-base font-bold "
+          onClick={handleSearchClick}
+        >
+          Search
+        </Button>
+      </div>
+
+      <div className="bg-[#4B5550] h-[35px] w-[170px] text-white gap-2 flex items-center rounded-2xl p-1">
+        <GpsFixedIcon />
+        <p>Current Location</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
