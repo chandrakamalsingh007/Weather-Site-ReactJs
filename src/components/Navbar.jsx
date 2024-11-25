e5c42506b2655137724805e3930b177875fc8e50
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2024-11-25 13:59:39 +0545

    Navbar UI completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import FilterDramaIcon from '@mui/icons-material/FilterDrama';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
 
 const Navbar = () => {
   return (
-    <nav>
-        <div className='flex items-center gap-2'>
-            <FilterDramaIcon/>
-            <p className="font-bold size-20">Hamro Weather</p>
+    <nav className='flex items-center justify-between p-5  '>
+        <div className='flex items-center gap-2 text-lg'>
+            <FilterDramaIcon className='text-[#4B5550]'/>
+            <p className="font-bold text-[#4B5550]">HamroWeather</p>
+        </div>
+
+        <div className="flex items-center gap-1">
+        <TextField className='bg-white rounded-3xl w-[22rem] outline-[#4B5550] ' variant="outlined" placeholder="Search city" />
+        <Button variant="contained" style={{backgroundColor:'#4B5550'}} className='rounded-md mt-1 text-base font-bold '>
+            Search
+        </Button>
+        </div>
+
+        <div className='bg-[#4B5550] h-[35px] w-[170px] text-white gap-2 flex items-center rounded-2xl p-1'>
+        <GpsFixedIcon/>
+        <p>Current Location</p>
         </div>
     </nav>
   )
