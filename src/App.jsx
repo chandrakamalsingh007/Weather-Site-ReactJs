2328048285b31a9d4b9624d60cfdf542d3565c5f
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2024-11-25 11:10:15 +0545

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import { useState,useContext } from 'react'
+import React from "react";
+import { useState, useContext } from "react";
+
 
 const App = () => {
+  const [input, setInput] = useState("");
+
   return (
-    <div className='text-xl font-bold text-center'>App</div>
-  )
-}
+    <div className="w-full h-screen text-white px-8">
+      <nav className="w-full p-3 flex justify-between items-center">
+        <h1 className="font-bold tracking-wide text-3xl">Weather App</h1>
+        <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
+          <img src="{}" alt="search" />
+          <input
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                //submit the form
+              }
+            }}
+            type="text"
+            className="focus:outline-none  text-[#212121] text-lg "
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default App
