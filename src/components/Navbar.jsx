6b663ec3cb74c8dc413664ca95d23c6c1dda2759
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2024-11-25 13:16:29 +0545

    integrating Tailwind css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FilterDramaIcon from '@mui/icons-material/FilterDrama';
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <FilterDramaIcon/>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
